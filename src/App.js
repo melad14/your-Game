a4b2fd32b4f6ad994ee2f561eb2e7bdf2484be01
melad14 <miladshehata513@gmail.com>
2023-10-13 19:14:49 +0300

    ssss

--- src/App.js
+++ src/App.js
@@ -71,6 +71,8 @@ function App() {
           <Route path='registration' element={<InverseProtectedRoute><Registration /></InverseProtectedRoute>} />
           <Route path='login' element={<InverseProtectedRoute><Login /></InverseProtectedRoute>} />
           <Route path='*' element={<NotFound />} />
+          <Route path='*' element={<Home />} />
+          
 
 
         </Route>
